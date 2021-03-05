33b64a0e85e59805c2d4f4bdf861c36a26aaee2d
dohaison <dohaison1998@gmail.com>
2021-03-05 18:54:57 +0700

    remove all loop func in smart contract

--- Contract/SmartFac.sol
+++ Contract/SmartFac.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.6.0;
 
 contract SmartFac {
   string private name;
+  address private initaddress;
   uint256 public total_of_reco = 0;
   uint256 public total_account = 0;
 
@@ -13,7 +14,7 @@ contract SmartFac {
   }
   mapping(uint256 => Reco) public reco;
 
-  mapping(uint256 => address) public trusted_acc;
+  mapping(address => bool) private trusted_acc;
 
   event added(
     int id,
@@ -24,60 +25,33 @@ contract SmartFac {
   constructor(string memory _name) public{
     require(bytes(_name).length > 0, "Missing name.");
     name = _name;
-    
-    trusted_acc[total_account] = msg.sender;
+    trusted_acc[msg.sender] = true;
+    initaddress = msg.sender;
     ++total_account;
   }
   
   function add_acc(address _add) public{
-      require(msg.sender == trusted_acc[0], "Plese use init contract Account.");
-      for(uint256 i=0; i<total_account; i++)
-        require(trusted_acc[i] != _add, "This address was existed.");
-        
-      trusted_acc[total_account] = _add;
+      require(msg.sender == initaddress, "Plese use init contract Account.");
+      require(! trusted_acc[_add], "This address was existed.");
+      trusted_acc[_add] = true;
       ++total_account;
   }
   
-  function remove_acc(address _add) public{
-      require(msg.sender == trusted_acc[0], "Plese use init contract Account.");
-      require(trusted_acc[0] != _add, "Cannot remove init Account.");
-      bool flag=false;
-      for(uint256 i=0; i<total_account; i++){
-          if(trusted_acc[i] == _add)
-            {
-                for(uint256 j=i; i<total_account-1; i++) {
-                    trusted_acc[j] = trusted_acc[j+1];
-                }
-                delete trusted_acc[total_account-1];
-                total_account -= 1;
-                flag=true;
-            }
-      }
-      require(flag == true, "This address not existed in list.");
+  function remove_acc(address _rm) public{
+      require(msg.sender == initaddress, "Plese use init contract Account.");
+      require(initaddress != _rm, "Cannot remove init Account.");
+      trusted_acc[_rm] = false;
   }
   
-  function Fac_name() public view virtual returns (string memory) {
-    return name;
-  }
+//   function Fac_name() public view virtual returns (string memory) {
+//     return name;
+//   }
   
-  function print_trusted_acc() public view returns (address[] memory) {
-    require(msg.sender == trusted_acc[0], "Plese use init contract Account.");
-    address[] memory memoryArray = new address[](total_account);
-    for(uint256 i=0; i<total_account; i++)
-        memoryArray[i] = trusted_acc[i];
-    return memoryArray;
-  }
-
   function new_Reco(int _id, uint256 _time, string memory _data) public {
-    bool trusted=false;
-    for(uint256 i=0; i<total_account; i++) {
-        if(trusted_acc[i] == msg.sender)
-            trusted=true;
-    }  
-    require(trusted==true, "This address not in trusted list.");
-    
+    require(trusted_acc[msg.sender], "This address not in trusted list.");
     ++total_of_reco;  
     reco[total_of_reco] = Reco(_id, _time, _data);
     emit added(_id, _time, _data);
   } 
 }
+
